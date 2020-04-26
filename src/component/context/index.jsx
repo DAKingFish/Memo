a34fd6e5b23e6bb88a9bae19422bfe43191cc8e7
KingFish <891248330@qq.com>
2020-04-26 12:31:13 +0800

    第一次更新

--- src/component/context/index.jsx
+++ src/component/context/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Input } from 'antd';
+import { Input } from 'antd'
+import { observer, inject } from 'mobx-react'
 import './index.less'
+const { TextArea } = Input;
 @inject('Mome')//注入
 @observer//申明使用mobx 观察者
-const { TextArea } = Input;
 class Context extends React.Component {//文本展示编辑插件
   render() {
     const { files , updateContent,context} = this.props.Memo  // 结构解析

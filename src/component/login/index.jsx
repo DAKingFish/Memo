76f2a8e51ac5cca35037579325db2445bcae424a
KingFish <891248330@qq.com>
2020-04-29 12:39:00 +0800

    增加路由

--- src/component/login/index.jsx
+++ src/component/login/index.jsx
@@ -5,8 +5,30 @@ import './index.less'
 @observer
 class Login extends React.Component {
   render() {
+    const { user: { name, pass }, changeValue } = this.props.User
     return <div className='app-login'>
-      我是登录页
+      <div className='login-name'>
+        <input placeholder='输入用户名' value={name} onChange={
+          (e) => {
+            changeValue('name', e.target.value)
+          }
+        } />
+      </div>
+      <div className='login-password'>
+        <input placeholder='请输入密码' value={pass} onChange={
+          (e) => {
+            changeValue('pass', e.target.value)
+            alert(pass)
+          }
+        } />
+      </div>
+      <div className='login-button'>
+        <button onClick={
+          () => {
+            alert(name, pass)
+          }
+        }>登录</button>
+      </div>
     </div>
   }
 }
--- src/store/user.js
+++ src/store/user.js
@@ -1,13 +1,17 @@
 import { observable, action } from "mobx";
 class User {
-  @observable isLogin = false
+  @observable isLogin = true
   @observable user = {
-    name: ''
+    name: '',
+    pass: ''
   }
   @action toLogin = () => { // 调登录接口
   }
+  @action changeValue = (key ,value) => {//箭头函数
+    this.user[key]= value
+  }
 }
-const user = new User() //创建mome类实际对象 
+const user = new User() //创建user类实际对象 
 export {
   user //抛出对象
 }

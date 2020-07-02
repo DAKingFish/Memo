36f5043cde47bb4745bea2bde012a68771bb0b3c
KingFish <891248330@qq.com>
2020-07-02 16:08:13 +0800

    第二次更新login页面

--- src/component/login/index.jsx
+++ src/component/login/index.jsx
@@ -12,7 +12,6 @@ class Login extends React.Component {
       <div className='login-body'>
         <div className='login-title'>用户登录</div>
         <div className='login-name'>
-          用户名<i className='iconfont icon-yonghuming'></i>:
           <input placeholder='输入用户名' value={name} onChange={
             (e) => {
               changeValue('name', e.target.value)
@@ -20,7 +19,6 @@ class Login extends React.Component {
           } />
         </div>
         <div className='login-password'>
-          密码<i className='iconfont icon-mima'></i>:
           <input placeholder='请输入密码' value={password} onChange={
             (e) => {
               changeValue('password', e.target.value)

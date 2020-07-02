8117c9066a0672e43ee63381a5b930538dbc7aa8
KingFish <891248330@qq.com>
2020-07-02 17:20:26 +0800

    更新login页面

--- src/component/login/index.jsx
+++ src/component/login/index.jsx
@@ -7,18 +7,21 @@ class Login extends React.Component {
   render() {
     const { user: { name, password }, changeValue, toLogin } = this.props.User
     return <div className='app-login'>
-      <div className='login-left'></div>
+      <div className='login-left'>
+        <i className='iconfont icon-jishiben1'></i>
+         <span className='icp'>©2020 JinYuanAI 皖ICP备19007022号</span>
+      </div>
       <div className='login-right'></div>
       <div className='login-body'>
         <div className='login-title'>用户登录</div>
-        <div className='login-name'>
+        <div className='login-input'>
           <input placeholder='输入用户名' value={name} onChange={
             (e) => {
               changeValue('name', e.target.value)
             }
           } />
         </div>
-        <div className='login-password'>
+        <div className='login-input'>
           <input placeholder='请输入密码' value={password} onChange={
             (e) => {
               changeValue('password', e.target.value)
